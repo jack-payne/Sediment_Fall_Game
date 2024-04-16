19df970fa3a899cc00fe912f766602abe0045bea
jack-payne <112025267+jack-payne@users.noreply.github.com>
2024-04-16 10:56:54 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Game from './Game';
+import PartTwo from './partTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Game />} />  // Set Game as the default component
+        <Route path="/partTwo" element={<PartTwo />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
